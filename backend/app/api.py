9f84915fed3cb8945c466efb3ba90478fa8d1c93
heisendelta <ariya8.narayan7@outlook.jp>
2025-07-30 15:51:18 +0900

    Made messages bold

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,108 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import numpy as np
 
-router = APIRouter()
+import spacy
+from wordfreq import zipf_frequency
 
-@router.get("/hello")
-def hello():
-    return {"message": "Hello from FastAPI!"}
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+import pickle
+from pathlib import Path
+import random
+
+# Constants (glove, zipf boundaries) loaded locally
+DATASET_PATH = Path(__file__).parent.parent / 'data'
+GLOVE_PATH = DATASET_PATH / 'glove_embeddings'
+
+with open(GLOVE_PATH / 'words.txt', 'r') as f:
+    words = f.read().splitlines()
+
+with open(GLOVE_PATH / 'glove.pkl', 'rb') as f:
+    glove = pickle.load(f)
+
+zipf_lower_bound = 4.0
+zipf_upper_bound = 5.0
+
+
+
+target_word = None
+similarity_ranking = None
+
+all_words = list(glove.keys())
+used_words = set()
+
+nlp = spacy.load('en_core_web_sm')
+def get_word_lemma(word):
+    return nlp(word)[0].lemma_
+
+def cosine_similarity_matrix(guess_vec, all_vecs):
+    guess_norm = np.linalg.norm(guess_vec)
+    all_norms = np.linalg.norm(all_vecs, axis=1)
+    dot_products = all_vecs @ guess_vec
+    return dot_products / (all_norms * guess_norm)
+
+def create_similarity_ranking(word1):
+    word1_vec = glove[word1]
+    all_vec = list(glove.values())
+
+    similarities = cosine_similarity_matrix(word1_vec, all_vec)
+    sorted_similarity_indices = np.argsort(-similarities).tolist()
+    return sorted_similarity_indices
+
+def get_ranking(word2):
+    if similarity_ranking is None:
+        raise TypeError('Similarity ranking uninitialized')
+
+    return similarity_ranking.index(all_words.index(word2)) + 1
+
+
+
+### API stuff
+
+class GuessRequest(BaseModel):
+    word: str
+
+@app.post('/start_game')
+def start_game():
+    global target_word, similarity_ranking
+
+    target_word = random.choice(words)
+    similarity_ranking = create_similarity_ranking(target_word)
+
+    return {'mesage': 'Game started', 'vocab_size': len(all_words)}
+
+@app.post('/guess')
+def guess(req: GuessRequest):
+    guess_word = req.word
+    original_word = guess_word
+    
+    zipf_score = zipf_frequency(guess_word, lang='en', wordlist='best')
+    if zipf_score > zipf_upper_bound:
+        return {'valid_guess': False, 'word': guess_word, 'message': 'too common.', 'rank': None}
+    elif zipf_score < zipf_lower_bound:
+        return {'valid_guess': False, 'word': guess_word, 'message': 'too uncommon.', 'rank': None}
+
+    if guess_word not in all_words:
+        word_lemma = get_word_lemma(guess_word)
+
+        if word_lemma in all_words:
+            guess_word = word_lemma
+        else:
+            return {'valid_guess': False, 'word': original_word, 'message': 'not found.', 'rank': None}
+        
+    if guess_word in used_words:
+        return {'valid_guess': False, 'word': original_word, 'message': 'already guessed.', 'rank': None}
+    used_words.add(guess_word)
+
+    ranking = get_ranking(guess_word)
+    return {'valid_guess': True, 'word': guess_word, 'message': f'ranked #{ranking}.', 'rank': ranking} # not using the mesasge currently
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from . import api
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from . import api
 
-app = FastAPI()
+# app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173"],  # Vite dev server
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-app.include_router(api.router)
+# app.include_router(api.router)
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True) # used to be "app.api:app"
